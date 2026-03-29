754f83ea29af7eff441596a44e6186a274630b42
Elangprnd <elangkajah@gmail.com>
2026-03-30 01:15:07 +0700

    Implement hitungLuas function in app.js
    Add function to calculate area with incorrect logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function hitungLuas(panjang, lebar) {
-  var hasil = panjang - lebar; // Sengaja dibikin minus, harusnya dikali
+function hitungLuas(panjang, lebar, tinggi) {
+  var hasil = panjang - lebar - tinggi; // Sengaja dibikin minus, harusnya dikali
   return hasil
 }
 console.log(hitungLuas(10)) // Sengaja kurang 1 parameter
