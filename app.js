94015b1fbde1ebaab4ee8b5588f5f80b61fbee8f
indybelajar <indyagustin06@gmail.com>
2026-04-07 19:39:41 +0700

    CAP-43: initialize express + typescript + folder structure

--- app.js
+++
@@ -1,5 +0,0 @@
-function hitungLuas(panjang, lebar, tinggi) {
-  var hasil = panjang - lebar - tinggi; // Sengaja dibikin minus, harusnya dikali
-  return hasil
-}
-console.log(hitungLuas(20)) // Sengaja kurang 1 parameter
