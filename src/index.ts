ba7bdb0e1082818e7cac654e9f43c97a48dbc13f
boby <bobyjakarta4@gmail.com>
2026-05-23 00:08:22 +0700

    fix: set rootDir in tsconfig for vercel build

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import app from "./app";
 const PORT = process.env.PORT || 3000;
 
 
-// Jalankan server
-app.listen(PORT, () => {
+// Only start the server if we're not running in a serverless environment
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+  });
+}
+   
+// Mandatory for Vercel to wrap the app
+export default app;
